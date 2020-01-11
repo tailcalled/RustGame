c100570f470d494f39e3717a5cb3c04e8d7d3475
Carl <tailcalled@gmail.com>
2020-01-11 20:44:23 +0100

    Added scenes, colors and removed warnings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use termion::input::TermRead;
-use std::io::{stdin, stdout, Write, Stdin, Stdout};
 use std::error::Error;
 use std::thread;
 use crossbeam::channel;
@@ -23,8 +21,6 @@ pub mod terminal;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let term = terminal::Terminal::new();
-    let mut stdin = stdin();
-    let mut stdout = stdout();
     term.println("Welcome to RustGame!")?;
     let username = term.readln("Please enter your username.")?;
     term.println(format!("Hello {}!", username))?;
@@ -38,7 +34,7 @@ fn main() -> Result<(), Box<dyn Error>>{
             thread::spawn(move || {
                 //host::host_game(rx);
             });
-            host_lobby(tx, stdin, stdout);
+            host_lobby(tx, term)?;
         }
         value if value.starts_with("join ") => {
 
@@ -49,19 +45,16 @@ fn main() -> Result<(), Box<dyn Error>>{
     Ok(())
 }
 
-fn host_lobby(tx: channel::Sender<LobbyCommand>, mut stdin: Stdin, mut stdout: Stdout) {
-    println!("Available commands:");
-    println!(" * start -- starts the game");
+fn host_lobby(tx: channel::Sender<LobbyCommand>, term: terminal::Terminal) -> Result<(), Box<dyn Error>> {
+    term.println("Available commands:")?;
+    term.println(" * start -- starts the game")?;
     loop {
-        print!("> ");
-        stdout.flush().unwrap();
-        let choice = TermRead::read_line(&mut stdin);
-        match choice.unwrap().as_ref().map(String::as_str) {
-            None => {},
-            Some("start") =>
+        let choice = term.readln("Enter a command.")?;
+        match choice.as_str() {
+            "start" =>
                 tx.send(LobbyCommand::StartGame).unwrap(),
-            Some(_) =>
-                println!("Invalid command!")
+            _ =>
+                term.println("Invalid command!")?
         }
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,11 +1,32 @@
 use termion;
 use termion::input::{TermRead};
 use termion::raw::{IntoRawMode};
-use std::io::{Stdout, stdout, Stdin, stdin, Write};
+use termion::color::AnsiValue;
+use std::io::{stdout, stdin, Write};
 use std::error::{Error};
 use std::thread;
 use std::sync::mpsc;
 use std::collections::VecDeque;
+
+#[derive(Copy, Clone)]
+struct ColoredChar {
+    ch: char,
+    foreground: AnsiValue,
+    background: AnsiValue,
+}
+
+pub struct Scene([[ColoredChar;SCREEN_H as usize];SCREEN_W as usize]);
+
+impl Default for Scene {
+    fn default() -> Scene {
+        let c = ColoredChar {
+            ch: ' ',
+            foreground: AnsiValue::rgb(5, 5, 5),
+            background: AnsiValue::rgb(0, 0, 0),
+        };
+        Scene([[c; SCREEN_H as usize]; SCREEN_W as usize])
+    }
+}
 
 enum TerminalCommand {
     Println(String),
@@ -13,6 +34,7 @@ enum TerminalCommand {
     AddReplyChar(char),
     Backspace,
     FinishReply(mpsc::Sender<String>),
+    DrawScene(Box<Scene>),
 }
 enum InputCommand {
     Query(String, mpsc::Sender<String>),
@@ -48,6 +70,10 @@ impl Terminal {
         self.input.send(InputCommand::Query(query.to_string(), rtx))?;
         Ok(rrx.recv()?)
     }
+    pub fn draw_scene(&self, scene: Scene) -> Result<(), Box<dyn Error>> {
+        self.control.send(TerminalCommand::DrawScene(Box::new(scene)))?;
+        Ok(())
+    }
 }
 
 const SCREEN_W: u16 = 60;
@@ -60,11 +86,13 @@ struct TerminalState {
     console_out: VecDeque<String>,
     query: Option<String>,
     reply: String,
+    scene: Scene,
 }
 impl TerminalState {
     fn render(&mut self) {
+        print!("{}{}", AnsiValue::rgb(5,5,5).fg_string(), AnsiValue::rgb(0, 0, 0).bg_string());
         print!("{}╔", termion::cursor::Goto(1, 1));
-        for i in 0 .. SCREEN_W+1+TERM_W {
+        for _ in 0 .. SCREEN_W+1+TERM_W {
             print!("═");
         }
         print!("╗");
@@ -76,36 +104,39 @@ impl TerminalState {
             }
             else if i == HEIGHT - 3 {
                 print!("{}", termion::cursor::Goto(1+1, 1+i+1));
-                for i in 0 .. SCREEN_W+1+TERM_W {
+                for _ in 0 .. SCREEN_W+1+TERM_W {
                     print!("─");
                 }
             }
         }
         print!("{}╚", termion::cursor::Goto(1, 2+HEIGHT));
-        for i in 0 .. SCREEN_W+1+TERM_W {
+        for _ in 0 .. SCREEN_W+1+TERM_W {
             print!("═");
         }
         print!("╝");
         self.finish_render();
         self.render_console();
-        self.render_query()
+        self.render_query();
+        self.render_scene();
     }
     fn render_console(&mut self) {
+        print!("{}{}", AnsiValue::rgb(5,5,5).fg_string(), AnsiValue::rgb(0, 0, 0).bg_string());
         for i in 0 .. TERM_H {
             print!("{}", termion::cursor::Goto(1+1+SCREEN_W+1, 1+1+i));
             let mut rem = TERM_W;
-            if (self.console_out.len() >= (TERM_H - i) as usize) {
+            if self.console_out.len() >= (TERM_H - i) as usize {
                 let line = &self.console_out[i as usize - (TERM_H as usize - self.console_out.len())];
                 print!("{}", line);
                 rem -= line.len() as u16;
             }
-            for i in 0 .. rem {
+            for _ in 0 .. rem {
                 print!(" ")
             }
         }
         self.finish_render();
     }
     fn render_query(&mut self) {
+        print!("{}{}", AnsiValue::rgb(5,5,5).fg_string(), AnsiValue::rgb(0, 0, 0).bg_string());
         print!("{}", termion::cursor::Goto(1+1, 1+1+SCREEN_H+1));
         let mut rem = (SCREEN_W+1+TERM_W) as usize;
         match &self.query {
@@ -115,7 +146,7 @@ impl TerminalState {
                 rem -= query.len();
             }
         }
-        for i in 0 .. rem {
+        for _ in 0 .. rem {
             print!(" ");
         }
         print!("{}", termion::cursor::Goto(1+1, 1+1+SCREEN_H+2));
@@ -126,9 +157,20 @@ impl TerminalState {
             print!("{}", self.reply);
             rem -= self.reply.len();
         }
-        for i in 0 .. rem {
+        for _ in 0 .. rem {
             print!(" ");
         }
+        self.finish_render();
+    }
+    fn render_scene(&mut self) {
+        let Scene(charmap) = self.scene;
+        for y in 0 .. SCREEN_H as usize {
+            print!("{}", termion::cursor::Goto(1+1, 1+1+y as u16));
+            for x in 0 .. SCREEN_W as usize {
+                print!("{}{}{}", charmap[x][y].foreground.fg_string(), charmap[x][y].background.bg_string(), charmap[x][y].ch);
+            }
+        }
+        print!("{}{}", AnsiValue::rgb(5,5,5).fg_string(), AnsiValue::rgb(0, 0, 0).bg_string());
         self.finish_render();
     }
     fn finish_render(&mut self) {
@@ -138,7 +180,7 @@ impl TerminalState {
         else {
             print!("{}", termion::cursor::Hide);
         }
-        stdout().flush();
+        stdout().flush().unwrap();
     }
     fn println(&mut self, line: String) {
         let iter = line.char_indices().step_by(TERM_W as usize).map(|(ix,_)| ix);
@@ -156,7 +198,7 @@ impl TerminalState {
         self.render_query();
     }
     fn add_reply_char(&mut self, ch: char) {
-        if self.reply.len() as (u16) < SCREEN_W+1+TERM_W-3 {
+        if (self.reply.len() as u16) < SCREEN_W+1+TERM_W-3 {
             self.reply.push(ch);
         }
         self.render_query();
@@ -166,9 +208,13 @@ impl TerminalState {
         self.render_query();
     }
     fn finish_reply(&mut self, resp: mpsc::Sender<String>) {
-        resp.send(std::mem::replace(&mut self.reply, String::new()));
+        resp.send(std::mem::replace(&mut self.reply, String::new())).unwrap();
         self.query = None;
         self.render_query();
+    }
+    fn draw_scene(&mut self, scene: Box<Scene>) {
+        self.scene = *scene;
+        self.render_scene();
     }
 }
 
@@ -177,9 +223,9 @@ fn terminal_thread(rx: mpsc::Receiver<TerminalCommand>) {
         console_out : VecDeque::new(),
         query : None,
         reply : "".to_string(),
+        scene : Scene::default(),
     };
     state.render();
-    let mut iter = 0;
     while let Ok(message) = rx.recv() {
         use crate::terminal::TerminalCommand::*;
         match message {
@@ -188,23 +234,23 @@ fn terminal_thread(rx: mpsc::Receiver<TerminalCommand>) {
             AddReplyChar(ch) => state.add_reply_char(ch),
             Backspace => state.backspace(),
             FinishReply(resp) => state.finish_reply(resp),
+            DrawScene(scene) => state.draw_scene(scene),
         }
-        iter += 1;
     }
 }
 
 
 fn input_thread(rx: mpsc::Receiver<InputCommand>, tx: mpsc::Sender<TerminalCommand>) {
     while let Ok(InputCommand::Query(query, result)) = rx.recv() {
-        tx.send(TerminalCommand::SetQuery(Some(query)));
-        let raw_mode = stdout().into_raw_mode();
+        tx.send(TerminalCommand::SetQuery(Some(query))).unwrap();
+        let _raw_mode = stdout().into_raw_mode();
         for ev in stdin().events() {
             use termion::event::*;
             match ev.unwrap() {
                 Event::Key(Key::Ctrl('c')) =>
                     std::process::abort(),
                 Event::Key(Key::Char('\n')) => {
-                    tx.send(TerminalCommand::FinishReply(result));
+                    tx.send(TerminalCommand::FinishReply(result)).unwrap();
                     break;
                 }
                 Event::Key(Key::Char(c)) =>
@@ -215,6 +261,6 @@ fn input_thread(rx: mpsc::Receiver<InputCommand>, tx: mpsc::Sender<TerminalComma
                 _ => {}
             }
         }
-        tx.send(TerminalCommand::SetQuery(None));
+        tx.send(TerminalCommand::SetQuery(None)).unwrap();
     }
 }
