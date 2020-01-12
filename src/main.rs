18b8c5dde1369052883903a09308e79de6a675a9
Carl <tailcalled@gmail.com>
2020-01-12 17:06:57 +0100

    Added support for spawning players

--- src/main.rs
+++ src/main.rs
@@ -83,4 +83,3 @@ fn main() -> Result<(), Box<dyn Error>>{
     }
     Ok(())
 }
-
--- src/world.rs
+++ src/world.rs
@@ -2,10 +2,17 @@ use rpds::RedBlackTreeMap as Map;
 use crate::ClientId;
 use crate::geom::*;
 use serde::{Serialize, Deserialize};
+use std::vec;
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 #[derive(Serialize, Deserialize)]
 pub struct EntityId(u64);
+
+impl EntityId {
+    fn next(self) -> Self {
+        EntityId(self.0 + 1)
+    }
+}
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct World {
@@ -22,12 +29,12 @@ impl Default for World {
     }
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Entity {
     pub pos: Vec,
     pub kind: EntityKind,
 }
-#[derive(Copy, Clone, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 pub enum EntityKind {
     Player(ClientId),
 }
@@ -39,6 +46,8 @@ pub enum PlayerActionEvent {
 #[derive(Debug, Serialize, Deserialize)]
 pub enum WorldEvent {
     PlayerAction(EntityId, PlayerActionEvent),
+    SpawnEntity(EntityId, Entity),
+    CreateEntity(Entity),
 }
 
 pub enum WorldError {
@@ -46,8 +55,9 @@ pub enum WorldError {
 }
 
 impl World {
-    pub fn handle_event(&self, sender: Option<ClientId>, ev: WorldEvent) -> Result<Self, WorldError> {
+    pub fn handle_event(&self, sender: Option<ClientId>, ev: WorldEvent) -> Result<(Self, vec::Vec<(u64, WorldEvent)>), WorldError> {
         let mut w = self.clone();
+        let mut evs = vec::Vec::new();
         use WorldEvent::*;
         match (sender, &ev) {
             (None, _) => {}
@@ -60,14 +70,27 @@ impl World {
                             _ => Err(WorldError::IllegalEvent)? // trying to move entity other than self -- unauthorized, fail
                         }
                 }
+            (Some(_), _) => Err(WorldError::IllegalEvent)?
         }
         match ev {
             PlayerAction(id, PlayerActionEvent::Move(dir)) =>
                 if w.get_entities_at(w.entities.get(&id).unwrap().pos + dir.to_vec()).next().is_none() {
                     w.entities.modify(id, |player| player.pos += dir.to_vec())
-                }
+                },
+            SpawnEntity(id, entity_data) =>
+                w.entities.insert_mut(id, entity_data),
+            CreateEntity(entity_data) => {
+                evs.push((0, SpawnEntity(w.next_entity_id, entity_data)));
+                w.next_entity_id = w.next_entity_id.next();
+            }
         }
-        Ok(w)
+        Ok((w, evs))
+    }
+    pub fn create_player_spawn_event(id: ClientId) -> WorldEvent {
+        WorldEvent::CreateEntity(Entity {
+            pos: Vec::new(0, 0),
+            kind: EntityKind::Player(id),
+        })
     }
     pub fn get_entities_at(&self, pos: Vec) -> impl Iterator<Item=(EntityId, &Entity)> {
         self.entities.iter().filter(move |(_, ent)| ent.pos == pos).map(|(eid, ent)| (*eid, ent))
